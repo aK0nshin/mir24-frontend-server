1017f8fe9d28de32d907cec6e8b1b1ca09324cb3
Pavel Belyaev <p.belyaev@gmail.com>
2016-06-09 17:55:38 +0300

    Articles many to many with Tags

--- app/Article.php
+++ app/Article.php
@@ -4,7 +4,10 @@ namespace mir24;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Article extends Model
-{
-    //
+class Article extends Model {
+
+    public function tags() {
+        return $this->belongsToMany('App\Tags','articles_tag','article_id','tag_id');
+    }
+
 }
