86b5bc484c669ef9541a1aaa39cdd11bd840e4a5
Konshin Alexander <kon2work@gmail.com>
2016-06-16 17:49:16 +0300

    Fix

--- database/migrations/2016_06_16_161556_video_src_to_text.php
+++ database/migrations/2016_06_16_161556_video_src_to_text.php
@@ -15,6 +15,8 @@ class VideoSrcToText extends Migration
         Schema::table('article_news', function ($table) {
         $table->text('video_id')->nullable()->change();
         $table->renameColumn('video_id', 'video_src');
+        $table->text('clip_id')->nullable()->change();
+        $table->renameColumn('clip_id', 'clip_src');
         });
     }
 
@@ -27,6 +29,8 @@ class VideoSrcToText extends Migration
     {
         Schema::table('article_news', function ($table) {
         DB::statement('ALTER TABLE article_news ALTER COLUMN video_src TYPE bigint USING (trim(video_src)::bigint);');
+        DB::statement('ALTER TABLE article_news ALTER COLUMN clip_src TYPE bigint USING (trim(clip_src)::bigint);');
+        $table->renameColumn('clip_src', 'clip_id');
         $table->renameColumn('video_src', 'video_id');
         });
     }
