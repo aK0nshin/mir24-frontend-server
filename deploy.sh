33d7b6a1db170a20fbdfe7c511ed567dfb4d666b
Konshin Alexander <kon2work@gmail.com>
2016-06-17 11:54:12 +0300

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -16,11 +16,13 @@ composer dump-autoload
 echo "database & migrations..."
 createuser -e -d -r -s -P -h localhost -p 5432 mir24
 createdb -e -U mir24 -h localhost -p 5432 mir24
-echo -n Password: 
-read -s password
+echo -n "Enter password again:": 
+read password
 echo
 sed -i -- "s/secret/$password/g" .env
 php artisan migrate
 php artisan db:seed
+echo "setting key..."
+php artisan key:generate
 
 echo "Complete"
