53de47ddd96256d7343bf35f4d648ee7c4508195
Alexander <kon2work@gmail.com>
2016-06-21 15:31:25 +0300

    Update News.php

--- app/News.php
+++ app/News.php
@@ -8,36 +8,36 @@ class News extends Model
 {
     public function relatedNews()
     {
-        return $this->belongsToMany('mir24\News', 'news_news', 'news_id', 'related_news');
+        return $this->belongsToMany('App\News', 'news_news', 'news_id', 'related_news');
     }
 
     public function creator()
     {
-        return $this->belongsTo('mir24\User', 'created_by');
+        return $this->belongsTo('App\User', 'created_by');
     }
 
     public function lastEditor()
     {
-        return $this->belongsTo('mir24\User', 'last_edit_by');
+        return $this->belongsTo('App\User', 'last_edit_by');
     }
 
     public function image()
     {
-        return $this->belongsTo('mir24\Image');
+        return $this->belongsTo('App\Image');
     }
 
     public function status()
     {
-        return $this->hasOne('mir24\Status', 'status');
+        return $this->hasOne('App\Status', 'status');
     }
 
     public function filials()
     {
-        return $this->belongsToMany('mir24\Filial');
+        return $this->belongsToMany('App\Filial');
     }
 
     public function tags()
     {
-        return $this->belongsToMany('mir24\Tag');
+        return $this->belongsToMany('App\Tag');
     }
 }
